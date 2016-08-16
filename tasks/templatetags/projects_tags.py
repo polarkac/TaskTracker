edb8d56351de013713d843a75083908894eeea15
Lukas Pohlreich <pohlreichlukas@gmail.com>
2016-08-16 14:33:17 +0200

    Fix for showing unpaid count

--- tasks/templatetags/projects_tags.py
+++ tasks/templatetags/projects_tags.py
@@ -1,7 +1,7 @@
 from django import template
-from django.db.models import Count
+from django.db.models import Prefetch
 
-from tasks.models import Project
+from tasks.models import Project, Task
 
 register = template.Library()
 
@@ -9,8 +9,11 @@ register = template.Library()
 def projects_list(user):
     projects = []
     if user.is_authenticated():
-        projects = Project.objects.filter(user=user).annotate(unpaid_tasks=Count(
-            'task'
-        ))
+        projects = Project.objects.filter(user=user).prefetch_related(
+            Prefetch(
+                'task_set', queryset=Task.objects.filter(paid=False),
+                to_attr='unpaid_tasks'
+            )
+        )
 
     return projects
